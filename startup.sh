5c2ce1d5c365b45dd2758fc7bba51c21ef64e230
rice-hill <heavenhayao2316@gmail.com>
2022-05-04 20:18:59 +0900

    修正: 不要なものを削除

--- startup.sh
+++ startup.sh
@@ -1,21 +1,5 @@
 #!/bin/sh
 # startup.sh
-
-set -e
-
 echo "startup.sh"
-
-if [ "$NODE_ENV" = "development" ]
-then
-  # develop環境の処理
-  until python manage.py migrate; do
-    echo "Postgres is unavailable - sleeping"
-    sleep 1
-  done
-  >&2 echo "Postgres is up - executing command"
-  python manage.py runserver 0.0.0.0:8000
-else
-  # それ以外の処理
-  # python manage.py migrate
-  python manage.py runserver 0.0.0.0:8000
-fi
+python manage.py migrate
+python manage.py runserver 0.0.0.0:8000
